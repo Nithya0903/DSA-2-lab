3a99d021808aa531bf2f16dc1125019c6592c8c7
Nithya Manoj <nithyamanoj.ms@gmail.com>
2020-02-05 04:30:01 +0530

    Update twoedge.py

--- dfs/twoedge.py
+++ dfs/twoedge.py
@@ -1,73 +1,59 @@
-
-class vertex :
-    def __init__(self,val) :
-        self.node=val
-        self.visited=0
-        self.st=0
-        self.ft=0
-        self.parent=0
-    def __str__(self) :
-        return str(self.node)+" "+str(self.st)+" "+str(self.ft)
-tim=0;
-G = [] 
-v=[]
-
-
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.st = 0
+        self.et = 0
+        self.parent = -1
+        self.visited = False
+time = 0
+v = []
 def min(i,j) :
     if(i>j) :
         return j
     else :
         return i
 
-def tec(s,ss) :
-    global tim
-    v[s].visited=1
-    v[s].st=tim;
-    tim=tim+1;
-    sst=v[s].st;
-    for u in G[s] :
+def twEdge(G,s):
+    global time
+    v[s].visited = True
+    v[s].st = time
+    sst = v[s].st
+    time = time+1
+    for u in G[s]:
         print(s,u)
-        if v[u].visited == 0 :
-            print("not visited :",s,u)
-            v[u].parent=s
-            sst=min(tec(u,ss),sst)
-        elif v[s].parent!=u :
-            print("visited :",u,v[u].parent)
-            sst=min(v[u].st,sst)
-    v[s].ft=tim
-    tim=tim+1
-    if sst==v[s].st and s!=ss :
-        print("not two edge connected")
-        exit(-1)
+        if v[u].visited == False:
+            v[u].parent = s
+            sst = min(twEdge(G,u),sst)
+        elif v[s].parent != u:
+            sst = min(v[u].st,sst)
+    v[s].ft =  time
+    time = time +1
+    print(sst)
+    if sst == v[s].st and v[s].st!=0:
+        print("Not 2 edge connected")
+        exit()
     return sst
 
 
-
-
 def main():
-    ''' Adjacency List representation. G is a list of lists. '''
-
+    G = []
     file=open('input.txt','r')
     for line in file:
-        line=line.strip()
-        adjacentVertices = []
-        first=True
-        for node in line.split(' '):
+        line = line.strip()
+        first = True
+        adjV = []
+        for nd in line.split():
             if first:
-                v.append(vertex(int(node)))
-                first=False
+                first = False
                 continue
-            adjacentVertices.append(int(node))
-        G.append(adjacentVertices)
-
+            adjV.append(int(nd))
+        G.append(adjV)
     file.close()
+    l = len(G)
     print(G)
-    s=input("\n source vertex")
-    s=int(s)
-    n=tec(s,s)
-    print("two edge connected")
-    #print("tree edge :",treeedge,"\nback edge :",backedge)
-
-
+    for i in range(l):
+        v.append(Node(i))
+    w = twEdge(G,0)
+    print("2  edge connected!")
 if __name__ == '__main__':
     main()
