7f9f2460e90c62a88aab119556d71204b911eec8
Nithya Manoj <nithyamanoj.ms@gmail.com>
2020-02-05 03:23:21 +0530

    Update dfs.py

--- dfs/dfs.py
+++ dfs/dfs.py
@@ -1,61 +1,65 @@
-
-class vertex :
-	def __init__(self,val) :
-		self.node=val
-		self.visited=0
-		self.st=0
-		self.ft=0
-		self.parent=0
-	def __str__(self) :
-		return str(self.node)+" "+str(self.st)+" "+str(self.ft)
-tim=0;
-G = [] 
-v=[]
-treeedge=[]
-backedge=[]
-vs=[]
-
-def dfs(s) :
-	global tim
-	v[s].visited=1
-	v[s].st=tim;
-	tim=tim+1;
-	for u in G[s] :
-		if v[u].visited == 0 :
-			treeedge.append([s,u])
-			v[u].parent=s
-			dfs(u)
-		elif v[s].parent!=u and v[u].parent!=u :
-			backedge.append([u,v[u].parent])
-	v[s].ft=tim
-	tim=tim+1
-	print(v[s])
+class Node:
+    def __init__(self,val):
+        self.st = 0
+        self.ft = 0
+        self.no = val
+        self.visited = False
+        self.parent = -1
+    def __str__(self):
+        return 'index: {}, start time:{}, end time:{}'.format(self.no,self.st,self.ft)
+time = 0 #to maintain timestamp
+v= [] #a list of class node instances
+tEdge =[]
+bEdge =[]
+def dfs(G,s):
+    global time
+    v[s].visited = True
+    v[s].st = time
+    time = time +1
+    print(s)
+    for u in G[s]:
+        if v[u].visited == False:
+            v[u].parent = s
+            tEdge.append([s,u])
+            dfs(G,u)
+        else:
+            if v[u].parent != s and v[s].parent !=u :
+                bEdge.append([v[u].parent,u])
+    v[s].ft = time
+    time = time +1
 
 
 
 
 def main():
-    ''' Adjacency List representation. G is a list of lists. '''
-
+    G = []
     file=open('input.txt','r')
     for line in file:
-        line=line.strip()
-        adjacentVertices = []
-        first=True
-        for node in line.split(' '):
+        line = line.strip()
+        adjV =[]
+        first = True
+        for nd in line.split(' '):
             if first:
-            	v.append(vertex(int(node)))
-            	first=False
-            	continue
-            adjacentVertices.append(int(node))
-        G.append(adjacentVertices)
-
+                first = False
+                continue
+            adjV.append(int(nd))
+        G.append(adjV)
     file.close()
     print(G)
-    s=input("\n source vertex")
-    s=int(s)
-    dfs(s)
-    print("tree edge :",treeedge,"\nback edge :",backedge)
+    for i in range(0,len(G)):
+        v.append(Node(i))
+    s = input("Enter src")
+    s = int(s)
+    print("Nodes in traversal order:")
+    dfs(G,s)
+    for i in range(len(G)):
+        print(v[i])
+    print("Tree edges")
+    for i in range(len(tEdge)):
+        print(tEdge[i])
+    print("Back edges")
+    for i in range(len(bEdge)):
+        print(bEdge[i])
 
 
 if __name__ == '__main__':
